b46e8315f21ee85334873e847a232e174085314a
Nikita Fomin <defdXs@yandex.ru>
2025-03-11 13:49:06 +0300

    help

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,16 +4,19 @@ import { ThemeProvider } from './react-envelope/contexts/ThemeContext';
 import { useAuth } from './react-envelope/hooks/useAuth';
 import { AuthContext } from './react-envelope/contexts/AuthContext';
 import { Router } from './components/utils/Router';
+import { SnackbarProvider } from 'notistack'
 
 function App() {
   const { user, login, logout, setUser } = useAuth();
 
   return (
+    <SnackbarProvider>
       <ThemeProvider>
         <AuthContext.Provider value={{ user, setUser }}>
           <Router/>
         </AuthContext.Provider>
       </ThemeProvider>
+    </SnackbarProvider>
   );
 }
 
--- frontend/src/components/utils/Router.jsx
+++ frontend/src/components/utils/Router.jsx
@@ -2,14 +2,16 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { LandingPage } from "../pages/LandingPage/LandingPage";
 import { PrivateRoute } from "../../react-envelope/utils/PrivateRoute";
 import { SamplesPage } from "../../react-envelope/components/pages/SamplesPage/SamplesPage";
+import { AuthPage } from "../../react-envelope/components/pages/AuthPage/AuthPage";
 
 export const Router = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<LandingPage/>}/>
+                <Route path='/login' element={<AuthPage/>}/>
                 <Route element={<PrivateRoute/>}>
-                    {/* <Route path='/lab' element={<SamplesPage/>}/> */}
+                    <Route path='/lab' element={<SamplesPage/>}/>
                 </Route>
             </Routes>
         </BrowserRouter>
