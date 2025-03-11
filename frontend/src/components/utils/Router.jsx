5a521b3c64a8ddf8e1e193715bab58f3c5db974a
Nikita Fomin <defdXs@yandex.ru>
2025-03-11 13:18:04 +0300

    gagaga

--- frontend/src/components/utils/Router.jsx
+++ frontend/src/components/utils/Router.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { LandingPage } from "../pages/LandingPage/LandingPage";
 import { PrivateRoute } from "../../react-envelope/utils/PrivateRoute";
+import { SamplesPage } from "../../react-envelope/components/pages/SamplesPage/SamplesPage";
 
 export const Router = () => {
     return (
@@ -8,7 +9,7 @@ export const Router = () => {
             <Routes>
                 <Route path='/' element={<LandingPage/>}/>
                 <Route element={<PrivateRoute/>}>
-                    <Route path='/lab' element={<div></div>}/>
+                    {/* <Route path='/lab' element={<SamplesPage/>}/> */}
                 </Route>
             </Routes>
         </BrowserRouter>
